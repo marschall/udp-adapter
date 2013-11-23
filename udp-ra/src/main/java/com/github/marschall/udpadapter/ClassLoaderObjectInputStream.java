424d5d12f4621bdfa6d0f24008769870102f3795
Philippe Marschall <philippe.marschall@gmail.com>
2013-11-23 17:51:50 +0100

    object input stream for Object message

---
+++ udp-ra/src/main/java/com/github/marschall/udpadapter/ClassLoaderObjectInputStream.java
@@ -0,0 +1,37 @@
+package com.github.marschall.udpadapter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+
+import net.sf.cglib.proxy.Proxy;
+
+final class ClassLoaderObjectInputStream extends ObjectInputStream {
+
+  private final ClassLoader classLoader;
+
+  ClassLoaderObjectInputStream(InputStream in, ClassLoader classLoader) throws IOException {
+    super(in);
+    this.classLoader = classLoader;
+  }
+  
+  @Override
+  protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException,
+      ClassNotFoundException {
+    String className = desc.getName();
+    return this.classLoader.loadClass(className);
+  }
+  
+  @Override
+  protected Class<?> resolveProxyClass(String[] interfaceNames) throws IOException,
+      ClassNotFoundException {
+    Class<?>[] interfaceClasses = new Class[interfaceNames.length];
+    for (int i = 0; i < interfaceNames.length; i++) {
+      String interfaceName = interfaceNames[i];
+      interfaceClasses[i] = this.classLoader.loadClass(interfaceName);
+    }
+    return Proxy.getProxyClass(this.classLoader, interfaceClasses);
+  }
+
+}
