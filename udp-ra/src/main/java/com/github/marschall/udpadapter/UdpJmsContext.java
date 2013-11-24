a33337f954a93aa66f65942d5de49f09f87e5217
Philippe Marschall <philippe.marschall@gmail.com>
2013-11-24 11:09:04 +0100

    add more methods

--- udp-ra/src/main/java/com/github/marschall/udpadapter/UdpJmsContext.java
+++ udp-ra/src/main/java/com/github/marschall/udpadapter/UdpJmsContext.java
@@ -1,5 +1,6 @@
 package com.github.marschall.udpadapter;
 
+import javax.jms.ConnectionMetaData;
 import javax.jms.JMSContext;
 import javax.resource.ResourceException;
 import javax.resource.spi.ResourceAdapter;
@@ -7,7 +8,24 @@ import javax.resource.spi.ResourceAdapterAssociation;
 
 abstract class UdpJmsContext implements JMSContext, ResourceAdapterAssociation {
 
+  private volatile String clientID;
+
   private volatile ResourceAdapter ra;
+  
+  @Override
+  public void setClientID(String clientID) {
+    this.clientID = clientID;
+  }
+  
+  @Override
+  public String getClientID() {
+    return this.clientID;
+  }
+  
+  @Override
+  public ConnectionMetaData getMetaData() {
+    return UdpConnectionMetaData.INSTANCE;
+  }
 
   @Override
   public void setResourceAdapter(ResourceAdapter ra) throws ResourceException {
