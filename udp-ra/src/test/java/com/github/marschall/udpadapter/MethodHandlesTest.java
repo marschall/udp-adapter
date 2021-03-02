aab8e2a8b83b8cd24076e8adce359d0ebaa25759
Philippe Marschall <philippe.marschall@gmail.com>
2021-03-02 20:39:56 +0100

    test method handle

--- udp-ra/src/test/java/com/github/marschall/udpadapter/MethodHandlesTest.java
+++ udp-ra/src/test/java/com/github/marschall/udpadapter/MethodHandlesTest.java
@@ -1,7 +1,13 @@
 package com.github.marschall.udpadapter;
 
+import static java.lang.invoke.MethodType.methodType;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -11,34 +17,43 @@ import org.junit.jupiter.api.Test;
 
 public class MethodHandlesTest {
 
+  private static final MethodHandle ON_MESSAGE;
+
+  static {
+    try {
+      ON_MESSAGE = MethodHandles.lookup().findVirtual(MessageListener.class, "onMessage", methodType(Void.TYPE, Message.class));
+    } catch (ReflectiveOperationException e) {
+      throw new AssertionError("could not look up MessageListener#onMessage");
+    }
+  }
+
   private boolean validatorCalled;
   private boolean listenerCalled;
-  private MethodHandle onMessage;
 
   @BeforeEach
   public void setUp() throws ReflectiveOperationException {
     this.validatorCalled = false;
     this.listenerCalled = false;
-    this.onMessage = MethodHandles.lookup().unreflect(MessageListener.class.getMethod("onMessage", Message.class));
   }
-  
+
   @Test
-  public void noException() {
-    
+  public void noException() throws Throwable {
+    assertFalse(listenerCalled);
+
+    MessageListener messageListener = new SampleMessageListener();
+    Message mesage = mock(Message.class);
+    ON_MESSAGE.invokeExact(messageListener, mesage);
+
+    assertTrue(listenerCalled);
   }
-  
-  public void check() {
-    
-  }
-  
+
   final class SampleMessageListener implements MessageListener {
 
     @Override
     public void onMessage(Message message) {
       listenerCalled = true;
-      
     }
-    
+
   }
 
 }
